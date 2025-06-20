978cb1b14b9b744f4c5ee0201f004739e98d6750
Keith Bentley <keithcbentley@msn.com>
2025-06-19 17:05:26 -0700

    Start CompileJob.

--- main.py
+++ main.py
@@ -7,20 +7,31 @@ from pathlib import Path
 
 g_build_dir:Path
 
-def make_build_dir():
+def make_build_dir() -> None:
     global g_build_dir
-    g_build_dir=Path(os.getcwd()) / "build/"
+    g_build_dir=Path(os.getcwd()) / "build"
     g_build_dir.mkdir(exist_ok=True)
 
 
-def figure_project_base_directory():
-    # check for given directory
-    # check for current working directory
-    # check that project file exists
-    # check that any auxiliary files exist
-    pass
+class CompileJob:
+    source_file_paths: list[Path]
+    output_directory_path: Path
 
-class Compiler:
+    def __init__(self):
+        self.source_file_paths = []
+
+    def add_source_file_path(self, source_file_path: Path) -> None:
+        self.source_file_paths.append(source_file_path)
+
+    def set_output_directory_path(self, output_directory_path: Path) -> None:
+        self.output_directory_path = output_directory_path
+
+    def print_source_paths(self) -> None:
+        for source_file_path in self.source_file_paths:
+            print(source_file_path)
+
+
+class CppCompiler:
 
     compiler_path:str = \
         "C:/Program Files/Microsoft Visual Studio/2022/Enterprise/SDK/ScopeCppSDK/vc15/VC/bin/cl.exe"
@@ -30,33 +41,30 @@ class Compiler:
     def __init__(self):
         pass
 
-    def write_output_directory(self, response_file: TextIO):
-        response_file.write('/Fo:"build/"\n')
+    @staticmethod
+    def write_output_directory(response_file: TextIO, output_directory_path: Path) -> None:
+        response_file.write(f'/Fo:"{output_directory_path}/"\n')
 
-    def write_source_files(self, response_file: TextIO):
-        response_file.write('"Main.Cpp"\n')
-        response_file.write('"StaticLibCode.Cpp"\n')
-        response_file.write('"DllCode.Cpp"\n')
+    @staticmethod
+    def write_source_files(response_file: TextIO, source_file_paths: list[Path]) -> None:
+        for source_file_path in source_file_paths:
+            response_file.write(f'"{source_file_path}"\n')
 
 
-    def generate_response_file(self):
+    def generate_response_file(self, compile_job: CompileJob) -> None:
         with open(self.response_filename, "wt") as response_file:
             response_file.write('/c\n')
-            self.write_output_directory(response_file)
-            self.write_source_files(response_file)
+            self.write_output_directory(response_file, compile_job.output_directory_path)
+            self.write_source_files(response_file, compile_job.source_file_paths)
 
 
-    def compile(self):
-        self.generate_response_file()
+    def compile(self, compile_job: CompileJob) -> None:
+        self.generate_response_file(compile_job)
         #print(self.compile_cmd)
         completed_process  = subprocess.run(self.compile_cmd,
                                                 stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
         print(completed_process.stdout.decode("utf-8"))
 
-
-class CompileJob:
-    def __init__(self):
-        pass
 
 
 
@@ -70,10 +78,12 @@ class StaticLibrarian:
     def __init__(self):
         pass
 
-    def write_output_library_name(self, response_file: TextIO):
+    @staticmethod
+    def write_output_library_name(response_file: TextIO):
         response_file.write('/OUT:"build/StaticLib.lib"\n')
 
-    def write_input_obj_names(self, response_file: TextIO):
+    @staticmethod
+    def write_input_obj_names(response_file: TextIO):
         response_file.write('"build/StaticLibCode.obj"\n')
 
     def generate_response_file(self):
@@ -98,13 +108,16 @@ class DllLinker:
     def __init__(self):
         pass
 
-    def write_dll_output_filename(self, response_file: TextIO):
+    @staticmethod
+    def write_dll_output_filename(response_file: TextIO):
         response_file.write('/OUT:"build/Dll.dll"\n')
 
-    def write_input_obj_filenames(self, response_file: TextIO):
+    @staticmethod
+    def write_input_obj_filenames(response_file: TextIO):
         response_file.write('"build/DllCode.obj"\n')
 
-    def write_standard_lib_filenames(self, response_file: TextIO):
+    @staticmethod
+    def write_standard_lib_filenames(response_file: TextIO):
         response_file.write(
             '"c:/Program Files/Microsoft Visual Studio/2022/Enterprise/SDK/ScopeCppSDK/vc15/VC/lib/libcmt.lib"\n')
         response_file.write(
@@ -145,13 +158,16 @@ class ExeLinker:
     def __init__(self):
         pass
 
-    def write_exe_output_filename(self, response_file: TextIO):
+    @staticmethod
+    def write_exe_output_filename(response_file: TextIO):
         response_file.write('/OUT:"build/Main.exe"\n')
 
-    def write_input_obj_filenames(self, response_file: TextIO):
+    @staticmethod
+    def write_input_obj_filenames(response_file: TextIO):
         response_file.write('"build/Main.obj"\n')
 
-    def write_standard_lib_filenames(self, response_file: TextIO):
+    @staticmethod
+    def write_standard_lib_filenames(response_file: TextIO):
         response_file.write(
             '"c:/Program Files/Microsoft Visual Studio/2022/Enterprise/SDK/ScopeCppSDK/vc15/VC/lib/libcmt.lib"\n')
         response_file.write(
@@ -182,26 +198,25 @@ class ExeLinker:
 
 
 def main():
+    print("current working directory: " + os.getcwd())
+    p:Path = Path(os.getcwd())
+
     make_build_dir()
-    compiler = Compiler()
-    compiler.compile()
+    compile_job = CompileJob()
+    compile_job.add_source_file_path(Path("Main.cpp"))
+    compile_job.add_source_file_path(Path("StaticLibCode.cpp"))
+    compile_job.add_source_file_path(Path("DllCode.cpp"))
+    compile_job.set_output_directory_path(Path("build/"))
+    compile_job.print_source_paths()
+
+    cpp_compiler = CppCompiler()
+    cpp_compiler.compile(compile_job)
     static_librarian = StaticLibrarian()
     static_librarian.make_static_lib()
     dll_linker = DllLinker()
     dll_linker.link()
     exe_linker = ExeLinker()
     exe_linker.link()
-    print("current working directory: " + os.getcwd())
-    p:Path = Path(os.getcwd())
-    print(" p: ", p, "  is_dir:", p.is_dir(), "  is_file:", p.is_file())
-    print("drive: ", p.drive)
-    print("root: ", p.root)
-    print("anchor: ", p.anchor)
-    print("parent: ", p.parent)
-    print("name: ", p.name)
-    print("suffix: ", p.suffix)
-    print("stem: ", p.stem)
-    print("is_absolute(): ", p.is_absolute())
 
 
 if __name__ == "__main__":
